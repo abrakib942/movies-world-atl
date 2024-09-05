8c379fcac989f93e25f73107c908a81da80b9adf
abrakib942 <abrakib942@gmail.com>
2024-09-05 09:02:05 +0600

    updated link

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -5,7 +5,8 @@ import { axiosBaseQuery } from "../../helpers/axios/axiosBaseQuery";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://movies-world-seven.vercel.app/api/v1",
+    // baseUrl: "http://localhost:5000/api/v1",
   }),
   endpoints: () => ({}),
   tagTypes: ["movie", "user"],
